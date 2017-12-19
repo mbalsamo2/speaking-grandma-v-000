3a0b719b8a228ee754e2356c3bebe272fb85ce4b
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-19 00:19:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  else
       return "NO, NOT SINCE 1938!"
   end
 end
